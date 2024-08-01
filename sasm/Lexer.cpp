8845dc72d5390b61a72205d3867e3e1012aa148d
Reza Tabrizi <rtabrizi03@gmail.com>
2024-08-01 07:33:16 -0400

    broken lexer

--- sasm/Lexer.cpp
+++ sasm/Lexer.cpp
@@ -63,18 +63,98 @@ strings Lexer::lex(std::string s ){
                 }
                 break;
             case READBLOCK:
+                if (s[i] == beg_char && s[i] != '"'){
+                    balance ++;
+                    lexeme[j] = s[i];
+                    i++;
+                    j++;
+                } else if (s[i] == end_char){
+                    balance --;
+                    lexeme[j] = s[i];
+                    i++;
+                    j++;
+                    if (balance <= 0){
+                        state = DUMP;
+                    }
+                } else if (end_char == '"' && s[i] == '\\'){
+                    // TODO: Fix this to actually record the chars
+                    i += 2;
+                } else {
+                    lexeme[j] = s[i];
+                    i++;
+                    j++;
+                }
                 break;
             case SKIP:
+                if (my_isspace(s[i])){
+                    i++;
+                } else {
+                    state = READCHAR;
+                }
                 break;
             case DUMP:
+                if (j > 0){
+                    lexeme[j] = 0;
+                    strlst.push_back(lexeme);
+                    j = 0;
+                }
+                state = START;
                 break;
             case COMMENT:
+                if (s[i] != '\n'){
+                    i++;
+                } else {
+                    state = READCHAR;
+                }
                 break;
             case END:
+                i = len;
                 break;
         }
     }
-
+    if (j > 0) {
+        lexeme[j] = 0;
+        strlst.push_back(lexeme);
+    }
     return strlst;
+}
 
+bool Lexer::my_isspace(char c) {
+    switch(c){
+        case '\n':
+        case '\r':
+        case '\t':
+        case '\v':
+        case ' ':
+        case '\f':
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool Lexer::isgroup(char c) {
+    beg_char = c;
+    switch (c) {
+        case '"':
+            end_char = '"';
+            return true;
+        case '(':
+            end_char = ')';
+            return true;
+        case ')':
+            return true;
+        default:
+            return false;
+    }
+}
+
+bool Lexer::isspecial(char c) {
+    switch(c){
+        case '[':
+        case ']':
+            return true;
+        default:
+            return false;
+    }
 }
